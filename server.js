bb397ca951f2cdb285ad9a412cee808ba315a1ce
Kristi Zentner <kristi.zentner17@gmail.com>
2021-09-13 20:22:29 +0200

    Updated server configs to allow requests from domain and return url to domain to redirect to stripe

---
+++ server.js
@@ -0,0 +1,33 @@
+const stripe = require('stripe')('sk_test_51JYwVoAOdPwZgIGSgPNdoGpzjOP4Dwd0rEiyXmbVTOfCOnP2LDc30yjao8pwRnLSdussGztOCzQmXv0doXB8QvyJ00axBuQ2KN');
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+const YOUR_DOMAIN = 'http://localhost:3000';
+
+const cors = require("cors");
+const corsOptions = {
+    origin: '*',
+    credentials: false,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
+app.use(express.json());
+
+app.post('/create-checkout-session', async (req, res) => {
+
+    const itemList = req.body.data;
+    let line_items = itemList.map(item => ({ price: item.price, quantity: parseInt(item.quantity) }));    
+
+    const session = await stripe.checkout.sessions.create({
+        line_items: line_items,
+        payment_method_types: [
+            'card',
+        ],
+        mode: 'payment',
+        success_url: `${YOUR_DOMAIN}/success.html`,
+        cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    });
+    res.json({ "url": session.url });
+});
+app.listen(4000, () => console.log('Running on port 4000'));
